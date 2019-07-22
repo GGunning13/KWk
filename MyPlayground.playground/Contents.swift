cf36d10819835a6df7ce00a892b180094f5ae6e1
Apple <apple@Apples-MacBook-Pro-91.local>
2019-07-22 11:30:11 -0700

    updated notes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
 var str = "Hello, playground"
+import UIKit
+
+var str = "Hello, playground"
+str
+var name = "grace"
+print ("hello world")
+var age = 13
+print(age)
+print (" I am \(age)")
+print ( "i am \(name)")
+var first = "karlie"
+var last = "kloss"
+print (" I am \(first) \(last)")
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+print (a+b+c+d)
+print ( (a+b+c+d) / 4)
+5<3
+12>7
+6 != 8
+//7 == "7"
+var dogAge = 1
+if dogAge < 2 {
+    print("you are a puppy")}
+var favoriteFood = "bread"
+if favoriteFood == "bread" {
+    print (" I really like bread")
+}
+else if favoriteFood != "bread" {
+    print ("I sort of like bread")
+}
+var gitHub = "rocks"
+if  gitHub == "rocks" {
+    print ("") }
